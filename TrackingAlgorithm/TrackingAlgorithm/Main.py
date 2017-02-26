ef8d07e918d4f2c3999d631f8ee642989ad358be
Michael Mell <michael.mell@gmx.net>
2017-02-26 12:48:00 +0100

    * Copied contents of tracking_002.py to Main.py and deleted it. * Renamed the file tracking_001.conf to MainTracking.conf.

---
+++ TrackingAlgorithm/TrackingAlgorithm/Main.py
@@ -0,0 +1,10 @@
+import sys
+#~ sys.path.append("/media/data_volume/mirrored_files/work/phd_thesis/matlab_functions/own/flicker_spectroscopy/image_interpolation/opencl_interpolation_testing/tracking_algorithm/dev_006/dev_SNAPSHOT_2014-03-07/")
+sys.path.append("/media/data_volume/mirrored_files/work/phd_thesis/matlab_functions/own/flicker_spectroscopy/image_interpolation/opencl_interpolation_testing/tracking_algorithm/dev_006/dev_SNAPSHOT_2014-03-12/")
+
+from contourTrackerMainClass import contourTrackerMain
+
+# run tracking
+prg = contourTrackerMain("tracking_001.conf",runInteractive=True)
+prg.initializeTracking()
+prg.track()
