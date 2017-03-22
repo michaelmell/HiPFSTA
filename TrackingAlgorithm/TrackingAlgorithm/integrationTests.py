f62fd435bfe4d625d05111d344d02673b16d76b2
Michael Mell <michael.mell@gmx.net>
2017-03-22 19:43:25 +0100

    Removed the sys.path.append statements.

--- TrackingAlgorithm/TrackingAlgorithm/integrationTests.py
+++ TrackingAlgorithm/TrackingAlgorithm/integrationTests.py
@@ -6,8 +6,6 @@ from contourTrackerMainClass import contourTrackerMain
 
 class Test_integrationTests(unittest.TestCase):
 	def test_CompleteProgramRun_000(self):
-		sys.path.append("/media/data_volume/mirrored_files/work/phd_thesis/matlab_functions/own/flicker_spectroscopy/image_interpolation/opencl_interpolation_testing/tracking_algorithm/dev_006/dev_SNAPSHOT_2014-03-12/")
-
 		prg = contourTrackerMain("MainTracking.conf",runInteractive=False)
 		prg.initializeTracking()
 		prg.track()
