integrationTest.py: Fixed tests that were failing due to the limited numerical precision, by using the function 'numpy.allclose' instead of testing '=='. testOpenClKernels.py: Added explicit parameter name 'atol', since there is an additional, optional parameter 'rtol', which comes first in the order of function parameters.

TrackingAlgorithm/TrackingAlgorithm/integrationTests.py
@@ -5,6 +5,8 @@ import numpy as np
 from contourTrackerMainClass import contourTrackerMain
 
 class Test_integrationTests(unittest.TestCase):
+	_equalityTolerance=1e-16
+
 	def test_CompleteProgramRun_000(self):
 		prg = contourTrackerMain("TrackingConfigs/TrackingConfig_000.conf",runInteractive=False)
 		prg.startTracking()
@@ -15,8 +17,8 @@ class Test_integrationTests(unittest.TestCase):
 		tmp=io.loadmat(dataAnalysisDirectoryReferencePath+'/contourCoordinatesY.mat')
 		contourCoordinatesYref = tmp['contourCoordinatesY']
 		
-		self.assertTrue(np.all(prg.contourCoordinatesX == contourCoordinatesXref))
-		self.assertTrue(np.all(prg.contourCoordinatesY == contourCoordinatesYref))
+		self.assertTrue(np.allclose(prg.contourCoordinatesX,contourCoordinatesXref,atol=self._equalityTolerance,equal_nan=False))
+		self.assertTrue(np.allclose(prg.contourCoordinatesY,contourCoordinatesYref,atol=self._equalityTolerance,equal_nan=False))
 
 	def test_CompleteProgramRun_001(self):
 		prg = contourTrackerMain("TrackingConfigs/TrackingConfig_002.conf",runInteractive=False)
@@ -28,8 +30,8 @@ class Test_integrationTests(unittest.TestCase):
 		tmp=io.loadmat(dataAnalysisDirectoryReferencePath+'/contourCoordinatesY.mat')
 		contourCoordinatesYref = tmp['contourCoordinatesY']
 		
-		self.assertTrue(np.all(prg.contourCoordinatesX == contourCoordinatesXref))
-		self.assertTrue(np.all(prg.contourCoordinatesY == contourCoordinatesYref))
+		self.assertTrue(np.allclose(prg.contourCoordinatesX,contourCoordinatesXref,atol=self._equalityTolerance,equal_nan=False))
+		self.assertTrue(np.allclose(prg.contourCoordinatesY,contourCoordinatesYref,atol=self._equalityTolerance,equal_nan=False))
 
 	def test_CompleteProgramRun_Tracking_Continuation_000(self):
 		prg = contourTrackerMain("TrackingConfigs/TrackingConfig_001.conf",runInteractive=False)
@@ -41,8 +43,8 @@ class Test_integrationTests(unittest.TestCase):
 		tmp=io.loadmat(dataAnalysisDirectoryReferencePath+'/contourCoordinatesY.mat')
 		contourCoordinatesYref = tmp['contourCoordinatesY']
 		
-		self.assertTrue(np.all(prg.contourCoordinatesX == contourCoordinatesXref))
-		self.assertTrue(np.all(prg.contourCoordinatesY == contourCoordinatesYref))
+		self.assertTrue(np.allclose(prg.contourCoordinatesX,contourCoordinatesXref,atol=self._equalityTolerance,equal_nan=False))
+		self.assertTrue(np.allclose(prg.contourCoordinatesY,contourCoordinatesYref,atol=self._equalityTolerance,equal_nan=False))
 
 	def test_CompleteProgramRun_Tracking_Continuation_001(self):
 		prg = contourTrackerMain("TrackingConfigs/TrackingConfig_003.conf",runInteractive=False)
@@ -54,8 +56,8 @@ class Test_integrationTests(unittest.TestCase):
 		tmp=io.loadmat(dataAnalysisDirectoryReferencePath+'/contourCoordinatesY.mat')
 		contourCoordinatesYref = tmp['contourCoordinatesY']
 		
-		self.assertTrue(np.all(prg.contourCoordinatesX == contourCoordinatesXref))
-		self.assertTrue(np.all(prg.contourCoordinatesY == contourCoordinatesYref))
+		self.assertTrue(np.allclose(prg.contourCoordinatesX,contourCoordinatesXref,atol=self._equalityTolerance,equal_nan=False))
+		self.assertTrue(np.allclose(prg.contourCoordinatesY,contourCoordinatesYref,atol=self._equalityTolerance,equal_nan=False))
 
 if __name__ == '__main__':
 	unittest.main()

TrackingAlgorithm/TrackingAlgorithm/testOpenClKernels.py
@@ -200,13 +200,13 @@ class Test_testOpenClKernels(unittest.TestCase):
 	def assertVectorEqualsExpectedResult(self,variable,referencePath):
 		outputValue = variable.get(self.queue)
 		referenceValue = np.load(referencePath)
-		self.assertTrue(np.allclose(outputValue, referenceValue,self._equalityTolerance, equal_nan=False))
+		self.assertTrue(np.allclose(outputValue, referenceValue,atol=self._equalityTolerance, equal_nan=False))
 
 	def assertVector2EqualsExpectedResult(self,variable,referencePath):
 		outputValue = variable.get(self.queue)
 		referenceValue = np.load(referencePath)
-		self.assertTrue(np.allclose(outputValue['x'], referenceValue['x'],self._equalityTolerance, equal_nan=False))
-		self.assertTrue(np.allclose(outputValue['y'], referenceValue['y'],self._equalityTolerance, equal_nan=False))
+		self.assertTrue(np.allclose(outputValue['x'], referenceValue['x'],atol=self._equalityTolerance, equal_nan=False))
+		self.assertTrue(np.allclose(outputValue['y'], referenceValue['y'],atol=self._equalityTolerance, equal_nan=False))
 
 	def loadHostVariable(self,variableName,path):
 		host_tmp = np.load(path+'/'+variableName+'.npy')