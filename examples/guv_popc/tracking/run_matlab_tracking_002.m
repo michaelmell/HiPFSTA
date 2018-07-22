cbbc1c0de96c73e9172f336a70fecd0016b9e441
Michael Mell <michael.mell@gmx.net>
2018-07-22 15:38:29 +0200

    Copied over the code for running the matlab tracking from C:\Private\PhD_Publications\Publication_of_Algorithm\2018-06-13__figure_10_reproduction_code\support_scripts\rbc_matlab_tracking

---
+++ examples/guv_popc/tracking/run_matlab_tracking_002.m
@@ -0,0 +1,6 @@
+%% This function only serves to start the analysis below
+restoredefaultpath;
+run('C:\Private\PhD_Publications\Publication_of_Algorithm\2018-06-13__figure_10_reproduction_code\matlab_functions\setPaths.m');
+addpath('C:\Private\PhD_Publications\Publication_of_Algorithm\2018-06-13__figure_10_reproduction_code\matlab_functions\own\flicker_spectroscopy\console_version\v003');
+trackImages('matlab_tracking_002.conf');
+% exit
