986ea5a92667958dfb69bef8ee3d83f666f5f190
Michael Mell <michael.mell@gmx.net>
2018-07-28 15:07:03 +0200

    Added Matlab script for running the RBC tracking with Pecreaux' algorithm.

---
+++ examples/rbc/tracking/run_matlab_tracking_002.m
@@ -0,0 +1,5 @@
+%% This function only serves to start the analysis below
+restoredefaultpath;
+run('C:\Private\PhD_Publications\Publication_of_Algorithm\Code\matlab_code\setPaths.m');
+addpath('C:\Private\PhD_Publications\Publication_of_Algorithm\Code\matlab_code\own\flicker_spectroscopy\console_version\v003');
+trackImages('matlab_tracking_002.conf');
