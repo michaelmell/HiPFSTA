c147cd55d72f3f1bf88a9948d72e1e76067a3a58
Michael Mell <michael.mell@gmx.net>
2018-07-31 22:03:29 +0200

    Paths in the Matlab tracking scripts are now relative.

--- examples/guv_popc/tracking/run_matlab_tracking_002.m
+++ examples/guv_popc/tracking/run_matlab_tracking_002.m
@@ -1,6 +1,3 @@
-%% This function only serves to start the analysis below
 restoredefaultpath;
-run('C:\Private\PhD_Publications\Publication_of_Algorithm\Code\matlab_code\setPaths.m');
-addpath('C:\Private\PhD_Publications\Publication_of_Algorithm\Code\matlab_code\own\flicker_spectroscopy\console_version\v003');
+addpath('../../../matlab_code/own/flicker_spectroscopy/console_version/v003');
 trackImages('matlab_tracking_002.conf');
-% exit
