8107029575ad833348995373050c8e64aa6f6ceb
Michael Mell <michael.mell@gmx.net>
2018-07-31 20:48:54 +0200

    Paths of the python tracking configs of the RBC dataset are now relative.

--- examples/rbc/tracking/run_tracking.py
+++ examples/rbc/tracking/run_tracking.py
@@ -1,10 +1,6 @@
 import sys
-#~ sys.path.append("/media/data_volume/mirrored_files/work/phd_thesis/matlab_functions/own/flicker_spectroscopy/image_interpolation/opencl_interpolation_testing/tracking_algorithm/dev_006/dev_SNAPSHOT_2014-03-07/")
-sys.path.append("C:/Private/PhD_Publications/Publication_of_Algorithm/Code/TrackingAlgorithm/TrackingAlgorithm/")
-
+sys.path.append("../../../TrackingAlgorithm/TrackingAlgorithm/")
 from contourTrackerMainClass import contourTrackerMain
 
-# run tracking
 prg = contourTrackerMain("tracking_config.conf",runInteractive=True)
 prg.startTracking()
-
