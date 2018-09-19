d8d7ac55c5ba61919346176ede6518c9acd6a4a8
Michael Mell <michael.mell@gmx.com>
2018-09-19 11:22:16 +0200

    Cleaned up scripts for generating the RBC figures.

--- examples/publication_figures/figure_10b_rbc/calculate_transforms_fourier_for_algorithm_comparison.m
+++ examples/publication_figures/figure_10b_rbc/calculate_transforms_fourier_for_algorithm_comparison.m
@@ -1,34 +1,18 @@
-%%
-%run('../../../matlab_code/setPaths.m');  
-%genpath('../../../matlab_code');
-pathBase = '../../../matlab_code/';
-addpath([pathBase,'/own/data_analysis/flickeringDataClass']);
-addpath([pathBase,'/external/hline_vline']);
-addpath([pathBase,'/own/figureSettings']);
-
-%% create RBC dataset
+%% setup environment
 close all;
 clear all;
-clear classes;
 
+pathBase = '../../../matlab_code/';
+addpath([pathBase,'/own/data_analysis/flickeringDataClass']);
+
+%% process RBC dataset
 basePath = '../../';
-% basePath = '/media/data_volume/figure_10_files/rbc/healthy/2014-05-05/';
 
-datasetPath = {'rbc/tracking/matlab_tracking_002/', ...
-               };
-datasetLabelAlternative = {'rbc_healthy_2014-05-05_rbc_4_movie_1_tracking_matlab_tracking_002', ...
-                          };
+datasetPath = {'rbc/tracking/matlab_tracking_002/'};
+datasetLabelAlternative = {'rbc_healthy_2014-05-05_rbc_4_movie_1_tracking_matlab_tracking_002'};
 
-% indices = (1:length(datasetLabelAlternative));
-% indices = (5:length(datasetLabelAlternative));
 indices = 1;
-% indices = (4:7);
-% indices = (2:3);
-
-% savePath = datasetPath;
-savePath = {[basePath,'/',datasetPath{1}], ...
-           };
-% indices = 5;
+savePath = {[basePath,'/',datasetPath{1}]};
 
 fourierSeriesFilenameEachCenter = 'fourierSeriesCenterOfEachContourCorrected.mat';
 fourierSeriesFilenameMeanCenter = 'fourierSeriesMeanCenterCorrected.mat';
@@ -53,9 +37,7 @@ for index = indices
     if( ~exist([savePath{index},'/',fourierSeriesFilenameEachCenter],'file'))
         dataset.setReferenceCenterMethod('forEachContour');
         dataset.calculatePolarCoordinates();
-    %     dataset.calculateFourierTransform();
         dataset.calculateFourierTransformNEW2();
-%         dataset.calculateFourierTransformNEW();
         fourierSeries = dataset.fourierseries;
         save([savePath{index},'/',fourierSeriesFilenameEachCenter],'fourierSeries','-mat');
     else
@@ -65,9 +47,7 @@ for index = indices
     if( ~exist([savePath{index},'/',fourierSeriesFilenameMeanCenter],'file'))
         dataset.setReferenceCenterMethod('meanCenter');
         dataset.calculatePolarCoordinates();
-    %     dataset.calculateFourierTransform();
         dataset.calculateFourierTransformNEW2();
-%         dataset.calculateFourierTransformNEW();
         fourierSeries = dataset.fourierseries;
         save([savePath{index},'/',fourierSeriesFilenameMeanCenter],'fourierSeries','-mat');
     else
@@ -81,24 +61,15 @@ for index = indices
     save([savePath{index},'/circumferenceSeries','.mat'],'circumferenceSeries','-mat');
 end
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%
-close all;
+%% process GUV dataset
 clear all;
-clear classes;
 
 fourierSeriesFilenameEachCenter = 'fourierSeriesCenterOfEachContourCorrected.mat';
 fourierSeriesFilenameMeanCenter = 'fourierSeriesMeanCenterCorrected.mat';
 
-basePath = {...
-            '../../'; ...
-            };
+basePath = {'../../';};
 
-basePaths = {...
-             basePath{1}, ...
-             };
+basePaths = {basePath{1}};
         
 datasetPath = { ...
                [basePath{1},'/','rbc/tracking/tracking_000/'], ...
@@ -113,66 +84,33 @@ end
            
 savePath = datasetPath;
 
-% mainName = 'rbc_';
 mainName = '';
 datasetLabel = {};
 for index = 1:length(datasetPath)
-    suffix = strrep(datasetPath{index},[basePaths{index},'/'],''); % remove basePath
+    suffix = strrep(datasetPath{index},[basePaths{index},'/'],'');
     suffix = strrep(suffix,'/','_');
     datasetLabel{index} = [mainName,suffix];
 end
 
-% idString = datasetLabel{index};
-% name = datasetLabelAlternative{index};
-
-% indexes = 1:length(datasetLabel);
-% indexes = 3:length(datasetLabel);
 indexes = [1];
 
-% pixelSizes = [50.0e-09,50.0e-09,50.0e-09,50.0e-09];
-
 for datasetIndex = indexes
-    % set parameters
+    % set processing parameters
     dataset = flickeringDataClass();
     dataset.setIdString(datasetLabel{datasetIndex});
     dataset.setLabelString(datasetLabel{datasetIndex});
     dataset.setResolution(50.0e-09);
-    % dataset.loadPythonTrackingSettings_v000( datasetPath );
-    % dataset.setNrOfModes(50);
-    % dataset.loadContours( datasetPath );
     dataset.setNrOfModes(1024);
     
     dataset.loadPythonTrackingContours_v000( datasetPath{datasetIndex} );
-%     dataset.testContours();
     dataset.calculateCircumference();
     dataset.calculateBarioCenter();
 
-    % create version with center for each profile
-%     dataset.setReferenceCenterMethod('forEachContour');
-%     dataset.calculatePolarCoordinates();
-%     dataset.calculateFourierTransformNEW();
-%     save([savePath{datasetIndex},'/',datasetLabel{datasetIndex},'_center_of_each_mode.mat'],'dataset','-mat');
-
-    %     dataset.loadImageInterpolationSettings_v002( path );
-    %     dataset.setResolution(50.0e-09);
-    %     dataset.setNrOfModes(50);
-    %     dataset.calculateCartesianCoordinates();
-
-    %
-%     % create version with averaged center of profiles
-%     dataset.setReferenceCenterMethod('meanCenter');
-%     dataset.calculatePolarCoordinates();
-%     dataset.calculateFourierTransformNEW2();
-%     save([savePath{datasetIndex},'/',datasetLabel{datasetIndex},'_averaged_center.mat'],'dataset','-mat');
-
-        % create version with center for each profile
     fileName = [savePath{datasetIndex},'/',fourierSeriesFilenameEachCenter];
     if( ~exist(fileName,'file'))
         dataset.setReferenceCenterMethod('forEachContour');
         dataset.calculatePolarCoordinates();
-    %     dataset.calculateFourierTransform();
         dataset.calculateFourierTransformNEW2();
-%         dataset.calculateFourierTransformNEW();
         fourierSeries = dataset.fourierseries;
         save(fileName,'fourierSeries','-mat');
     else
@@ -183,9 +121,7 @@ for datasetIndex = indexes
     if( ~exist(fileName,'file'))
         dataset.setReferenceCenterMethod('meanCenter');
         dataset.calculatePolarCoordinates();
-    %     dataset.calculateFourierTransform();
         dataset.calculateFourierTransformNEW2();
-%         dataset.calculateFourierTransformNEW();
         fourierSeries = dataset.fourierseries;
         save(fileName,'fourierSeries','-mat');
         radiusSeries = dataset(1).getRadiusSeries;
